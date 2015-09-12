df6cf77ce350c339bf622f4a9e053608d4053b5c
Asuman Suenbuel <asuman.suenbuel@sap.com>
2015-09-12 00:00:44 -0700

    added some command line related things

---
+++ process_argv.py
@@ -0,0 +1,43 @@
+#
+# process the command line arguments of the simulator
+#
+
+import getopt, os, sys
+
+class CommandLine:
+
+    def __init__(self,argv=sys.argv[1:]):
+        self.argv = argv
+        self.options = {}
+
+    def parse(self):
+        try:
+            opts, args = getopt.getopt(self.argv,"hd:",["dataFolder="])
+        except getopt.GetoptError as e:
+            print str(e)
+            return False
+        
+        for opt,arg in opts:
+            if opt in ("-d","--dataFolder"):
+                self.options['defaultDataFolder'] = arg
+
+        self.args = args
+        if len(args) > 1:
+            print "too many command line arguments: " + str(args[1:])
+            return False
+
+        if len(args) > 0:
+            arg = args[0]
+            if os.path.exists(arg):
+                self.fileArgument = arg
+            else:
+                print '"' + arg + '" does not exist.'
+                return False
+
+        return True
+
+    def hasoption(self,option):
+        return option in self.options
+
+    def getoption(self,option):
+        return self.options[option]
